b78c13b5cdfa8e5a569b38f71e9b88fcd5054209
Kishore-G-QOS <kishore.g@quickonlinesolutions.com>
2026-09-02 16:09:49 +0530

    exceptions and middlewares

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from sqlalchemy import text
 
+from app.middlewares.request_id import add_request_id
+from sqlalchemy.exc import IntegrityError
+from app.exceptions.handlers import integrity_error_handler
+
 from app.api.v1.auth import router as auth_router
 from app.api.v1.users import router as users_router
 from app.api.v1.admin_users import router as admin_users_router
@@ -17,10 +21,20 @@ app.include_router(auth_router, prefix="/api/v1")
 app.include_router(users_router, prefix="/api/v1")
 app.include_router(admin_users_router, prefix="/api/v1")
 
+# Middleware
+app.middleware("http")(add_request_id)
+
+# Exceptions Handlers
+app.add_exception_handler(
+    IntegrityError,
+    integrity_error_handler
+)
+
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
 
 @app.get("/")
 async def root():
