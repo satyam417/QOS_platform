22d4a46552af66fa88c95dbed8a8719b558948a3
Manasa0208-hub <manasa.p@quickonlinesolutions.com>
2026-09-02 19:06:19 +0530

    Customer profile (#13)
    * Add customer login register and OTP APIs
    
    * Add customer profile and address APIs and requirements
    
    * Add customer profile and address APIs
    
    * Add refresh token support
    
    * Update authentication and customer profile APIs
    
    * latest change
    
    * Resolve merge conflicts with main
    
    * Resolve main.py merge conflict
    
    ---------
    
    Co-authored-by: Kishore-G-QOS <kishore.g@quickonlinesolutions.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,11 @@ from app.exceptions.handlers import integrity_error_handler
 from app.api.v1.auth import router as auth_router
 from app.api.v1.users import router as users_router
 from app.api.v1.admin_users import router as admin_users_router
+from app.api.v1.customer import router as customer_router
+from app.api.v1.vendors import router as vendors_router
+from app.api.v1.operators import router as operators_router
+from app.api.v1.categories import router as categories_router
+from app.api.v1.services import router as services_router
 
 
 app = FastAPI(
@@ -20,14 +25,21 @@ app = FastAPI(
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(users_router, prefix="/api/v1")
 app.include_router(admin_users_router, prefix="/api/v1")
+app.include_router(customer_router)
+app.include_router(vendors_router, prefix="/api/v1")
+app.include_router(operators_router, prefix="/api/v1")
+app.include_router(categories_router, prefix="/api/v1")
+app.include_router(services_router, prefix="/api/v1")
+
 
 # Middleware
 app.middleware("http")(add_request_id)
 
+
 # Exceptions Handlers
 app.add_exception_handler(
     IntegrityError,
-    integrity_error_handler
+    integrity_error_handler,
 )
 
 
